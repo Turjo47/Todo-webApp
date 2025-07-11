04ab168aea6819776acc66aebcd32e83bd9f0f76
Turjo47 <turjo47@gmail.com>
2025-07-11 11:22:44 +0600

    models_todoApp

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+PRIORITY_CHOICES = [
+    ('low','Low'),
+    ('med', 'Medium'),
+    ('high', 'High'),
+]
+
+STATUS_CHOICES= [
+    ('pending', 'Pending'),
+    ('in_progress', 'In Progress'),
+    ('completed', 'Completed'),
+]
+class Task(models.Model):
+    user = models.ForeignKey(on_delete=models.CASCADE)
+    title= models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    is_completed = models.BooleanField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES, default='med')
+    
--- tasks/views.py
+++ tasks/views.py
@@ -4,12 +4,4 @@ from django.http import HttpResponse
 todo_items=[]
 # Create your views here.
 def todo_view(request):
-    global todo_items
-
-    if request.method == 'POST':
-        task = request.POST.get('task')
-        if task:
-            todo_items.append(task)
-        
-        return redirect ('todo')
-    return render(request, 'task/todo.html',{'task':todo_items})
+    return render(request, "tasks/task_list.html")
