58d628cb365ad56f0af16fd6cc938fe47d35cc13
Turjo47 <turjo47@gmail.com>
2025-07-12 15:15:27 +0600

    template name change

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.todo_list, name = "task_list"),
+    path('', views.task_list, name = "task_list"),
     path('create/', views.task_create, name="task_create"),
     path('edit/<int:task_id>', views.task_edit, name="task_edit"),
     path('delete/<int:task_id>', views.task_delete, name="task_delete"),
